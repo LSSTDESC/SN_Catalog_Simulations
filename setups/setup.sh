23d10ce30e76de9d1e66936d1bbba12056d18994
pgris <philippe.gris@clermont.in2p3.fr>
2018-05-22 05:07:20 -0700

    Modifiy setup

--- setups/setup.sh
+++ setups/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+#setup lsst_sims
+source /global/common/software/lsst/cori-haswell-gcc/stack/setup_w_2018_13-sims_2_7_0.sh
+setup lsst_sims
+
 export PYTHONPATH=Sim_SNCosmo:$PYTHONPATH
-export PYTHONPATH=Sim_SNSim/:$PYTHONPATH
-export PYTHONPATH=Sim_SNAna/:$PYTHONPATH
+export PYTHONPATH=Sim_SNSim:$PYTHONPATH
+export PYTHONPATH=Sim_SNAna:$PYTHONPATH
+
+export PYTHONPATH=../SN_Utils/Utils:$PYTHONPATH
