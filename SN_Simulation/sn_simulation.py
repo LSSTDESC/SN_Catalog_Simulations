86e2c57ee81b13dbd2856e926be45b86dd10435a
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2018-10-09 08:43:41 +0200

    comments+pep

--- SN_Simulation/sn_simulation.py
+++ SN_Simulation/sn_simulation.py
@@ -12,11 +12,12 @@ from astropy.table import vstack, Table, Column
 import os
 import h5py
 
+
 class Simu_All:
 
-    def __init__(self, cosmo_par, tel_par,sn_parameters,
-                 save_status,outdir,prodid,
-                 simu_config, display_lc,names):
+    def __init__(self, cosmo_par, tel_par, sn_parameters,
+                 save_status, outdir, prodid,
+                 simu_config, display_lc, names):
 
         self.cosmo_par = cosmo_par
         self.sn_parameters = sn_parameters
@@ -32,16 +33,16 @@ class Simu_All:
                                  w0=cosmo_par['w0'], wa=cosmo_par['wa'])
 
         self.telescope = Telescope(name=tel_par['name'],
-                                    throughput_dir=tel_par['throughput_dir'],
-                                    atmos_dir=tel_par['atmos_dir'],
-                                    atmos=tel_par['atmos'],
-                                    aerosol=tel_par['aerosol'],
-                                    airmass=tel_par['airmass'])
+                                   throughput_dir=tel_par['throughput_dir'],
+                                   atmos_dir=tel_par['atmos_dir'],
+                                   atmos=tel_par['atmos'],
+                                   aerosol=tel_par['aerosol'],
+                                   airmass=tel_par['airmass'])
 
         if self.save_status:
-            self.Prepare_Save(outdir,prodid)
-           
-    def Prepare_Save(self,outdir,prodid):
+            self.Prepare_Save(outdir, prodid)
+
+    def Prepare_Save(self, outdir, prodid):
 
         if not os.path.exists(outdir):
             print('Creating output directory', outdir)
@@ -55,7 +56,7 @@ class Simu_All:
         # -> name : LC_fieldname_fieldid_season.hdf5
         self.simu_out = outdir+'/Simu_'+prodid+'.hdf5'
         self.lc_out = outdir+'/LC_'+prodid+'.hdf5'
-        self.sn_meta=[]
+        self.sn_meta = []
         # and these files will be removed now (before processing)
         # if they exist (to avoid confusions)
         if os.path.exists(self.simu_out):
@@ -65,16 +66,15 @@ class Simu_All:
 
     def __call__(self, tab):
 
-        all_obs = Observations(data=tab,names=self.names)
+        all_obs = Observations(data=tab, names=self.names)
         for season in range(len(all_obs.seasons)):
             obs = all_obs.seasons[season]
             # remove the u band
             idx = [i for i, val in enumerate(obs['band']) if val[-1] != 'u']
-            self.Process_Season(obs[idx],season)
-           
-        
-    def Process_Season(self, obs,season):
-        
+            self.Process_Season(obs[idx], season)
+
+    def Process_Season(self, obs, season):
+
         gen_params = self.gen_par(obs)
         for i, val in enumerate(gen_params[:]):
             self.index_hdf5 += 1
@@ -82,8 +82,10 @@ class Simu_All:
             for name in ['z', 'X1', 'Color', 'DayMax']:
                 sn_par[name] = val[name]
             SNID = sn_par['Id']+self.index_hdf5
-            sn_object = SN_Object(
-                self.simu_config, sn_par, self.cosmology, self.telescope, SNID)
+            sn_object = SN_Object(self.simu_config,
+                                  sn_par,
+                                  self.cosmology,
+                                  self.telescope, SNID)
 
             module = import_module(self.simu_config['name'])
             simu = module.SN(sn_object, self.simu_config)
@@ -92,9 +94,10 @@ class Simu_All:
             if self.save_status:
                 # write this table in the lc_out
 
-                obs_table.write(
-                    self.lc_out, path='lc_'+str(self.index_hdf5),
-                    append=True, compression=True)
+                obs_table.write(self.lc_out,
+                                path='lc_'+str(self.index_hdf5),
+                                append=True,
+                                compression=True)
                 # append the parameters in tab_out
                 m_lc = obs_table.meta
                 self.sn_meta.append((m_lc['SNID'], m_lc['Ra'],
@@ -102,17 +105,16 @@ class Simu_All:
                                      m_lc['X1'], m_lc['Color'],
                                      m_lc['z'], self.index_hdf5, season))
 
-
     def Finish(self):
         if len(self.sn_meta) > 0:
             Table(rows=sn_meta,
                   names=['SNID', 'Ra', 'Dec', 'DayMax', 'X1',
-                         'Color','z', 'id_hdf5', 'season'],
+                         'Color', 'z', 'id_hdf5', 'season'],
                   dtype=('i4', 'f8', 'f8', 'f8', 'f8', 'f8',
                          'f8', 'i4', 'i4')).write(
                              self.simu_out, 'summary', compression=True)
 
-            
+
 parser = argparse.ArgumentParser(
     description='Run a SN simulation from a configuration file')
 parser.add_argument('config_filename',
@@ -128,40 +130,48 @@ def run(config_filename):
     cosmo_par = config['Cosmology']
     # load telescope
     tel_par = config['Instrument']
-  
+
     # this is for output
 
     save_status = config['Output']['save']
     outdir = config['Output']['directory']
     prodid = config['ProductionID']
-    # sn parameters        
+    # sn parameters
     sn_parameters = config['SN parameters']
 
     simu_config = config['Simulator']
     display_lc = config['Display']
 
-    names=dict(zip(['band','mjd','rawSeeing','sky','exptime','moonPhase','Ra','Dec','Nexp','fiveSigmaDepth','seeing','airmass','night','season'],['band','mjd','seeingFwhm500','sky','exptime','moonPhase','Ra','Dec','numExposures','fiveSigmaDepth','seeingFwhmEff','airmass','night','season']))
-    
-    simu = Simu_All(cosmo_par, tel_par,sn_parameters,
-                    save_status,outdir,prodid,
-                    simu_config, display_lc,names=names)
-    
+    names = dict(zip(['band', 'mjd', 'rawSeeing', 'sky', 'exptime',
+                      'moonPhase', 'Ra', 'Dec', 'Nexp', 'fiveSigmaDepth',
+                      'seeing', 'airmass', 'night', 'season'],
+                     ['band', 'mjd', 'seeingFwhm500', 'sky', 'exptime',
+                      'moonPhase', 'Ra', 'Dec', 'numExposures',
+                      'fiveSigmaDepth', 'seeingFwhmEff', 'airmass',
+                      'night', 'season']))
+
+    simu = Simu_All(cosmo_par, tel_par, sn_parameters,
+                    save_status, outdir, prodid,
+                    simu_config, display_lc, names=names)
+
     # load input file (.npy)
 
     input_name = config['Observations']['dirname'] + \
-                 '/'+config['Observations']['filename']
-    print('loading',input_name)
+        '/'+config['Observations']['filename']
+    print('loading', input_name)
     input_data = np.load(input_name)
-    
+
     print(input_data.dtype)
 
- 
-
-    for (fieldname,fieldid) in np.unique(input_data[['fieldname','fieldid']]):
-        idx = (input_data['fieldname'] == fieldname) & (input_data['fieldid'] == fieldid) 
+    toprocess = np.unique(input_data[['fieldname', 'fieldid']])
+    print('Number of simulations', len(toprocess))
+    for (fieldname, fieldid) in toprocess:
+        idx = (input_data['fieldname'] == fieldname) & (
+            input_data['fieldid'] == fieldid)
         simu(input_data[idx])
 
     simu.Finish()
+
 
 def main(args):
     print('running')
