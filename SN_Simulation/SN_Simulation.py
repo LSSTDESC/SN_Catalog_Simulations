0c1c048149592ce22f4efa48435a0f08f9b8b0cd
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2018-10-22 12:55:47 +0200

    Extract SN_Simulation for the run script

--- SN_Simulation/SN_Simulation.py
+++ SN_Simulation/SN_Simulation.py
@@ -1,5 +1,3 @@
-import yaml
-import argparse
 from SN_Telescope import Telescope
 from astropy.cosmology import w0waCDM
 from SN_Object import SN_Object
@@ -13,7 +11,7 @@ import os
 import h5py
 import multiprocessing
 
-class Simu_All:
+class SN_Simulation:
     """ Main class for simulation
     Input (init)
     ---------
@@ -180,75 +178,3 @@ class Simu_All:
                          'f8', 'i4', 'i4','S3','i8','i8','f8')).write(
                              self.simu_out, 'summary', compression=True)
 
-
-parser = argparse.ArgumentParser(
-    description='Run a SN simulation from a configuration file')
-parser.add_argument('config_filename',
-                    help='Configuration file in YAML format.')
-
-
-def run(config_filename):
-    # YAML input file.
-    config = yaml.load(open(config_filename))
-    print(config)
-
-    # load cosmology
-    cosmo_par = config['Cosmology']
-    # load telescope
-    tel_par = config['Instrument']
-
-    # this is for output
-
-    save_status = config['Output']['save']
-    outdir = config['Output']['directory']
-    prodid = config['ProductionID']
-    # sn parameters
-    sn_parameters = config['SN parameters']
-
-    simu_config = config['Simulator']
-    display_lc = config['Display']
-
-    names = dict(zip(['band', 'mjd', 'rawSeeing', 'sky', 'exptime',
-                      'moonPhase', 'Ra', 'Dec', 'Nexp', 'fiveSigmaDepth',
-                      'seeing', 'airmass', 'night', 'season', 'pixarea',
-                      'pixRa', 'pixDec'],
-                     ['band', 'mjd', 'seeingFwhm500', 'sky', 'exptime',
-                      'moonPhase', 'Ra', 'Dec', 'numExposures',
-                      'fiveSigmaDepth', 'seeingFwhmEff', 'airmass',
-                      'night', 'season', 'pixarea',
-                     'pixRa', 'pixDec']))
-
-    simu = Simu_All(cosmo_par, tel_par, sn_parameters,
-                    save_status, outdir, prodid,
-                    simu_config, display_lc, names=names,nproc=config['Multiprocessing']['nproc'])
-
-    # load input file (.npy)
-
-    input_name = config['Observations']['dirname'] + \
-        '/'+config['Observations']['filename']
-    print('loading', input_name)
-    input_data = np.load(input_name)
-
-    print(input_data.dtype)
-
-    toprocess = np.unique(input_data[['fieldname', 'fieldid']])
-    print('Number of fields to simulate', len(toprocess))
-    for (fieldname, fieldid) in toprocess:
-        idx = (input_data['fieldname'] == fieldname) & (
-            input_data['fieldid'] == fieldid)
-        print('Simulating',fieldname,fieldid)
-        simu(input_data[idx],fieldname,fieldid)
-        
-    simu.Finish()
-
-
-def main(args):
-    print('running')
-    time_ref = time.time()
-    run(args.config_filename)
-    print('Time', time.time()-time_ref)
-
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    main(args)
